d7b251d14d3e868f6adad16ac8d600b3afb05147
BuddhaThisBread <40405216+BuddhaThisBread@users.noreply.github.com>
2021-02-23 14:50:09 -0600

    Add files via upload
    Part 1 of Lab4

---
+++ Lab4.java.ino
@@ -0,0 +1,32 @@
+char Alpha = 'A';
+char Number = '0';
+
+unsigned long Timer;
+
+void setup() {
+  Serial.begin(9600);
+  Timer = millis();
+}
+
+void loop() {
+  if (millis() - Timer >= 500) {
+    Serial.print(Alpha);
+    Serial.print(Number);
+    Alpha += 1;
+    Number += 1;
+    if ( Alpha > 'Z')
+      Alpha = 'A';
+
+    if (Number > '9')
+      Number = '0';
+      
+    Timer += 500;
+  }
+    
+  if (Serial.available() > 0) {
+    if (Serial.read() == 'R') {
+      Alpha = 'A';
+      Number = '0';
+    }
+  }
+}
