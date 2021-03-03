93d5db9360625ba544f62e55c9f239ee6472875b
BuddhaThisBread <40405216+BuddhaThisBread@users.noreply.github.com>
2021-03-03 09:57:29 -0600

    Update Lab05.java

--- Lab05.java
+++ Lab05.java
@@ -1,52 +1,58 @@
 /*#include <LiquidCrystal.h>
 
+// initialize display
+LiquidCrystal LcdDriver(A5, A4, 5, 6, 7, 8);
+
+// create timer, iterates every second
+int count = 0; 
+long sTimer = millis();
+
 void setup() {
-  // initialize display
-  LiquidCrystal LcdDriver(11, 9, 5, 6, 7, 8);
   LcdDriver.begin(16,2);
   LcdDriver.clear();
-  // create timer, iterates every second
-  int count = 0; 
-  long sTimer = millis();
   }
 
 void loop() {
   if (millis() - sTimer >= 1000) {
     count++;
     // print count which incremints every second
-    LcdDriver.print(count, BIN);
+    LcdDriver.print(count);
     sTimer += 1000;
   }
-}*/
+}
+
+
+*/
 
 #include <LiquidCrystal.h>
 
+LiquidCrystal LcdDriver(A5, A4, 5, 6, 7, 8);
+
+// create timer, iterates every second
+int count = 0; 
+long sTimer = millis();
+long stopWatch = sTimer;
+int count2 = 0;
+
 void setup() {
-  // initialize display
-  LiquidCrystal LcdDriver(11, 9, 5, 6, 7, 8);
   LcdDriver.begin(16,2);
   LcdDriver.clear();
-  // create timer, iterates every second
-  int count = 0; 
-  long sTimer = millis();
-  long stopWatch = sTimer;
-  int count2 = 0;
   }
 
 void loop() {
   if (millis() - sTimer >= 1000) {
     count++;
     // print count which incremints every second
-    LcdDriver.print(count, BIN);
+    LcdDriver.print(count);
     sTimer += 1000;
   }
   if (millis() - stopWatch >= 100) {
     if (digitalRead(4) == HIGH) {
       count2 = 0;
     } else if (digitalRead(4) == LOW) {
-      count2++;
       LcdDriver.clear();
-      LcdDriver.print(count2,BIN);
+      count2++;
+      LcdDriver.print(count2);
     }
     stopWatch += 100;
   }
